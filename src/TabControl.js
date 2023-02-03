66315e814087a0e8735316c7867e6c4b198bf8f2
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-03 12:41:06 +1000

    initial commit

--- src/TabControl.js
+++ src/TabControl.js
@@ -1,41 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-const TabControl = ({ tabs, className }) => {
+const TabControl = ({ tabs }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const tabListRef = useRef(null);
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        setSelectedIndex((selectedIndex - 1 + tabs.length) % tabs.length);
+        break;
+      case "ArrowRight":
+        setSelectedIndex((selectedIndex + 1) % tabs.length);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <div className={className}>
-      <div className="tab-list" role="tablist">
+    <div className="tab-control">
+      <div
+        className="tab-list"
+        role="tablist"
+        aria-orientation="horizontal"
+        ref={tabListRef}
+      >
         {tabs.map((tab, index) => (
-          <div
-            className={`tab ${selectedIndex === index ? "selected" : ""}`}
-            key={`tab-${index}`}
+          <button
+            className="tab"
+            role="tab"
+            aria-selected={selectedIndex === index}
+            aria-controls={`panel-${index}`}
+            id={`tab-${index}`}
+            tabIndex={selectedIndex === index ? 0 : -1}
+            key={tab.label}
+            onClick={() => setSelectedIndex(index)}
+            onKeyDown={handleKeyDown}
           >
-            <button
-              role="tab"
-              aria-selected={selectedIndex === index}
-              aria-controls={`panel-${index}`}
-              onClick={() => setSelectedIndex(index)}
-            >
-              {tab.label}
-            </button>
-          </div>
+            {tab.label}
+          </button>
         ))}
       </div>
-      <div className="tab-content">
-        {tabs.map((tab, index) => (
-          <div
-            role="tabpanel"
-            id={`panel-${index}`}
-            aria-labelledby={tab.label}
-            hidden={selectedIndex !== index}
-            className="tab-panel"
-            key={`panel-${index}`}
-          >
-            {tab.content}
-          </div>
-        ))}
-      </div>
+      {tabs.map((tab, index) => (
+        <div
+          tabIndex="0"
+          role="tabpanel"
+          hidden={selectedIndex !== index}
+          id={`panel-${index}`}
+          aria-labelledby={`tab-${index}`}
+          key={tab.label}
+        >
+          {tab.content}
+        </div>
+      ))}
     </div>
   );
 };
