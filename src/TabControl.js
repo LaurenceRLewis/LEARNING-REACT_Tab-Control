91845d41586ff48cf5686ebf6173cee2ba4fe4d8
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-03 13:00:39 +1000

    Attempt at fixing double tab issue

--- src/TabControl.js
+++ src/TabControl.js
@@ -26,7 +26,7 @@ const TabControl = ({ tabs }) => {
         ref={tabListRef}
       >
         {tabs.map((tab, index) => (
-          <button
+          <div
             className="tab"
             role="tab"
             aria-selected={selectedIndex === index}
@@ -38,7 +38,7 @@ const TabControl = ({ tabs }) => {
             onKeyDown={handleKeyDown}
           >
             {tab.label}
-          </button>
+          </div>
         ))}
       </div>
       {tabs.map((tab, index) => (
